9b57e0d597fbc66e4c59a4dae6a1d85ade53b8c3
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-02-01 14:32:36 +0100

    inicio

---
+++ script/script.js
@@ -0,0 +1,42 @@
+const questions = ["Em qual dia de novembro o seu escorpião favorito nasceu?",
+  "E ele tem Pai... Qual o nome do pincel dessa obra de arte?",
+  "Qual o meu quinto sobrenome?",
+  "Qual o meu super heroi favorito?",
+  "Qual a minha altura?"
+];
+const choicesArray = [
+  ["05.11.1997", "11.11.1998", "02.11.1997", "07.11.1998"],
+  ["Luiz", "Ricardo", "Miriano", "Mario"],
+  ["Ribeiro", "Santiago", "Silva", "Bastos"],
+  ["Batman", "Pantera Negra", "Wolverine", "Homem Aranha"],
+  ["1.73m", "1.74m", "1.75m", "1.76m"]
+];
+const correctAnswers = ["02.11.1997", "Mario", "Ribeiro", "Wolverine", "1.74m"];
+let currentQuestionIndex = 0;
+let score = 0;
+
+function displayQuestion() {
+  if (currentQuestionIndex < questions.length) {
+    document.getElementById('question').innerHTML = questions[
+      currentQuestionIndex];
+    for (let i = 0; i < 4; i++) {
+    const btn = document.getElementById(`choice${i+1}`);
+    btn.innerHTML = choicesArray[currentQuestionIndex][i];
+    btn.value = choicesArray[currentQuestionIndex][i];
+  }
+    } else {
+    document.getElementById('result').innerHTML =
+      `You scored ${score} out of ${questions.length}!`;
+      document.getElementById('question').innerHTML = "";
+      document.getElementById('choices').innerHTML = "";
+  }
+}
+
+function checkAnswer(button) {
+  if (button.value === correctAnswers[currentQuestionIndex]) {
+        score++;
+    }
+    currentQuestionIndex++;
+    displayQuestion();
+}
+displayQuestion();
